a4f72d5812b1681edd31e97d553c2cb669e6da06
Formerscholar <2319315514@qq.com>
2021-06-26 13:19:38 +0800

    修复bug

--- api/src/main/java/com/chad/api/dao/pojo/Article.java
+++ api/src/main/java/com/chad/api/dao/pojo/Article.java
@@ -5,43 +5,38 @@ import lombok.Data;
 @Data
 public class Article {
 
-  public static final int Article_TOP = 1;
+	private Long id;
 
-  public static final int Article_Common = 0;
+	private String title;
 
+	private String summary;
 
-  private Long id;
+	private Integer commentCounts;
 
-  private String title;
+	private Integer viewCounts;
 
-  private String summary;
+	/**
+	 * 作者id
+	 */
+	private Long authorId;
+	/**
+	 * 内容id
+	 */
+	private Long bodyId;
+	/**
+	 * 类别id
+	 */
+	private Long categoryId;
 
-  private Integer commentCounts;
-
-  private Integer viewCounts;
-
-  /**
-   * 作者id
-   */
-  private Long authorId;
-  /**
-   * 内容id
-   */
-  private Long bodyId;
-  /**
-   * 类别id
-   */
-  private Long categoryId;
-
-  /**
-   * 置顶
-   */
-  private Integer weight = Article_Common;
+	/**
+	 * 置顶
+	 */
+	private Integer weight;
 
 
-  /**
-   * 创建时间
-   */
-  private Long createDate;
+	/**
+	 * 创建时间
+	 */
+	private Long createDate;
 
 }
